41b759433428ebeb85ccc901dd5baec6cc62810f
홍은표 <44293278+potato179@users.noreply.github.com>
2022-11-21 20:35:28 +0900

    타이틀 화면

--- rpggame/index.c
+++ rpggame/index.c
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <conio.h>
 #include <time.h>
+
 #pragma comment(lib, "winmm.lib")
 
 void gotoxy(int x, int y) {
@@ -25,42 +26,62 @@ void resizeConsole() {
 	system(chTemp);
 }
 
-void displayStart() {
-	while (1) {
-		if (_kbhit()) {
-			_getch();
-			break;
-		}
-
-		gotoxy(50, 10);
-		printf("몰래 라면 먹기 게임");
-		gotoxy(39, 13);
-		printf("--------------- 게임 규칙 ---------------");
-		gotoxy(30, 15);
-		printf("1. 호실에서 라면을 조이며 먹는다.");
-		gotoxy(30, 16);
-		printf("2. 복도로 나가 휴지통에 라면을 버린다.");
-		gotoxy(30, 17);
-		printf("3. 생활관 교무실에서 나오는 빨모쌤한테 잡히지 않게 튄다.");
-		gotoxy(30, 18);
-		printf("4. 샤워실로 피신하면 된다.");
-		gotoxy(50, 23);
-		printf("라면먹기");
-		gotoxy(50, 24);
-		printf("반성하기");
-
-		gotoxy(48, 23);
-		printf("▶");
-
-		gotoxy(1, 25);
-	}
-}
-
 void printBound() {
 	for (int y = 0; y < 30; y++) {
 		for (int x = 0; x < 60; x += 1) {
 			if (x == 0 || y == 0 || x == 59 || y == 29) printf("■");
 			else printf("  ");
+		}
+	}
+}
+
+int selectNum = 1;
+
+void displayStart() {
+	gotoxy(50, 10);
+	printf("몰래 라면 먹기 게임");
+	gotoxy(39, 13);
+	printf("--------------- 게임 규칙 ---------------");
+	gotoxy(30, 15);
+	printf("1. 호실에서 라면을 조이며 먹는다.");
+	gotoxy(30, 16);
+	printf("2. 복도로 나가 휴지통에 라면을 버린다.");
+	gotoxy(30, 17);
+	printf("3. 생활관 교무실에서 나오는 빨모쌤한테 잡히지 않게 튄다.");
+	gotoxy(30, 18);
+	printf("4. 샤워실로 피신하면 된다.");
+	gotoxy(50, 23);
+	printf("라면먹기");
+	gotoxy(50, 24);
+	printf("반성하기");
+
+	gotoxy(48, 23);
+	printf("▶");
+
+	gotoxy(1, 25);
+
+	while (1) {
+		if (_kbhit()) {
+			char key = _getch();
+			if (key == 72 || key == 80) {
+				if (selectNum == 1) {
+					selectNum = 2;
+					gotoxy(48, 23);
+					printf("  ");
+					gotoxy(48, 24);
+					printf("▶");
+				}
+				else {
+					selectNum = 1;
+					gotoxy(48, 24);
+					printf("  ");
+					gotoxy(48, 23);
+					printf("▶");
+				}
+			}
+			if (key == '\r') {
+				break;
+			}
 		}
 	}
 }
@@ -72,4 +93,12 @@ int main() {
 	resizeConsole();
 	printBound();
 	displayStart();
+
+	if (selectNum == 1) {
+		system("cls");
+		PlaySound(NULL, NULL, NULL);
+		PlaySound(TEXT("gayjoygo.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+		printf("gayjoygo~");
+	}
+	else return 0;
 }
